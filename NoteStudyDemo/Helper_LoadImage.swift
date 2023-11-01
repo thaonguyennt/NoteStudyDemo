9f4b393bef97efbb6de48cd16458f68d96fe58c0
thaonguyennt <thaonguyennt1804@gmail.com>
2023-11-01 22:38:56 +0700

    udate searrch func

--- NoteStudyDemo/Helper_LoadImage.swift
+++ NoteStudyDemo/Helper_LoadImage.swift
@@ -13,40 +13,35 @@ class Helper_LoadImage {
     static let shared = Helper_LoadImage()
     let key = "AIzaSyDGgSCAOtnbiMwtklpJJ1VlacodhiM2E1c"
     let cx = "93c4fe5686c554061"
+    let searchType = "image"
+
     func search(text: String){
         let service = GTLRService()
-        let url = URL(string: "https://www.googleapis.com/customsearch/v1?q=\(text)&cx=\(cx)&tbm=isch")
-                let query = GTLRQuery(pathURITemplate: "", httpMethod: nil, pathParameterNames: nil)
-//        service.executeQuery(query) { ticket, object, error in
-//            if let err = error {
-//                print(err)
-//                return
-//            }
-//        }
-        service.fetchObject(with: URL(string:  "https://www.googleapis.com/customsearch/v1?q=\(text)&cx=\(cx)&tbm=isch&key=\(key)")!, objectClass: nil, executionParameters: nil) { ticket, object, error in
-            if let err = error {
-                print(err)
-                return
+        let url = URL(string:  "https://www.googleapis.com/customsearch/v1?q=\(text)&cx=\(cx)&searchType=\(searchType)&tbm=isch&key=\(key)")
+        if let encodeQuery = text.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+            if let urlString = url {
+                let task = URLSession.shared.dataTask(with: urlString) { data, _, error in
+                    if let error = error {
+                        print(error.localizedDescription)
+                    }
+                    do {
+                        guard let data = data else { return }
+                        let jsonData = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                        if let items = jsonData?["items"] as? [[String: Any]], let firstItem = items.first, let link = firstItem["link"] as? String, let imageURL = URL(string: link) {
+                            print(url)
+                        } else {
+                            print("Image URL not found")
+                        }
+                        
+                    } catch {
+                        print(error.localizedDescription)
+
+                    }
+                }
+                task.resume()
             }
-//            let item = GTLRDataObject(json: convertDictionaryToJSON(object as! [String : Any]))
-            ticket.fetchedObject?.json
-            ticket.
-            let response = object as! GTLRObject
-            print(ticket.fetchedObject?.json)
-            
+
         }
-              
-        
-        //        service.executeQuery(query) { [weak self] (ticket, object, error)  in
-        //
-        //            if let err = error {
-        //                print(err)
-        //                return
-        //            }
-        //            let response = object as! GTLRObject
-        //
-        //        }
-        
     }
     func convertDictionaryToJSON(_ dictionary: [String: Any]) -> String? {
 
